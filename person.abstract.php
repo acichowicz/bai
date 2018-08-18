bef342c3cf6e914ca4472869576fac11adb38dce
Konrad Maslowiec <kmaslowiec@gmail.com>
2018-08-18 12:44:27 +0200

    Functions added
    Added

--- person.abstract.php
+++ person.abstract.php
@@ -11,4 +11,19 @@ abstract class Person implements PersonInterface {
 	public function getLastName() {
 		return $this->lastName;
 	}
+        
+        public function saveData() {
+        if (!file_exists("data")) {
+            mkdir("data");
+        }
+        file_put_contents("data/" . className . ".counter.txt", $this->counter);
+    }
+
+    public function getCounter() {
+        if (file_exists("data/" . className . ".counter.txt")) {
+            $this->counter = file_get_contents("data/" . className . ".counter.txt");
+        }
+        $this->counter = $this->counter + 1;
+        echo $this->counter;
+    }
 }
