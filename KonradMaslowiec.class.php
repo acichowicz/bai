de775099a2b093c11f70b7f921e01843bcdbb1bc
Konrad Maslowiec <kmaslowiec@gmail.com>
2018-08-18 12:38:00 +0200

    First Branch
    Uploaded

--- KonradMaslowiec.class.php
+++ KonradMaslowiec.class.php
@@ -2,12 +2,35 @@
 
 require_once 'person.abstract.php';
 
-class KonradMaslowiec extends Person{
-    
-    protected $firstName = "Konrad";
-    protected $lastName = "Maslowiec";
-    
+define('className', "KonradMaslowiec");
+
+class KonradMaslowiec extends Person {
+
+    protected $firstName = 'Konrad';
+    protected $lastName = 'Maslowiec';
+    protected $counter = 0;
+
+    public function saveData() {
+        if (!file_exists("data")) {
+            mkdir("data");
+        }
+
+        file_put_contents("data/" . className . ".counter.txt", $this->counter);
+    }
+
+    public function getCounter() {
+        if (file_exists("data/" . className . ".counter.txt")) {
+            $this->counter = file_get_contents("data/" . className . ".counter.txt");
+        }
+        $this->counter = $this->counter + 1;
+        echo $this->counter;
+    }
+
 }
+
+    
+    
+
 
 /* 
  * To change this license header, choose License Headers in Project Properties.
