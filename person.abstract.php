591e526cf429a1a5bfba87ed192e4d8f0a90c84d
klimowiczdawid <40468078+klimowiczdawid@users.noreply.github.com>
2018-08-18 12:43:15 +0200

    functions moved to abstract

--- person.abstract.php
+++ person.abstract.php
@@ -1,14 +1,36 @@
 <?php
+
 require_once 'person.interface.php';
 
 abstract class Person implements PersonInterface {
-	protected $firstName;
-	protected $lastName;
-	public function getFirstName() {
-		return $this->firstName;
-	}
+    
 
-	public function getLastName() {
-		return $this->lastName;
-	}
+    protected $firstName;
+    protected $lastName;
+    protected $counter;
+
+    public function getFirstName() {
+        return $this->firstName;
+    }
+
+    public function getLastName() {
+        return $this->lastName;
+    }
+
+    public function saveData() {
+        if (file_exists("data") == FALSE) {
+            mkdir("data");
+        }
+
+        file_put_contents("data/" . className . ".counter.txt", $this->counter);
+    }
+
+    public function getCounter() {
+        if (file_exists("data/" . className . ".counter.txt")) {
+            $this->counter = file_get_contents("data/" . className . ".counter.txt");
+        }
+        $this->counter = $this->counter + 1;
+        echo $this->counter;
+    }
+
 }
