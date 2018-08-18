16151c85ba51922d4fa98111b9030f6ad17afc99
klimowiczdawid <40468078+klimowiczdawid@users.noreply.github.com>
2018-08-18 11:47:17 +0200

    blabla

--- DawidKlimowicz.class.php
+++ DawidKlimowicz.class.php
@@ -2,23 +2,26 @@
 
 require_once 'person.abstract.php';
 
-define('className',"DawidKlimowicz");
+define('className', "DawidKlimowicz");
 
 class DawidKlimowicz extends Person {
 
     protected $firstName = 'Dawid';
     protected $lastName = 'Klimowicz';
-    protected $counter;
-    
-    public function saveData(){
-        if(file_exists("data") == FALSE){
+    protected $counter = 0;
+
+    public function saveData() {
+        if (file_exists("data") == FALSE) {
             mkdir("data");
         }
-        
-        file_put_contents("data/".className.".counter.txt", $this->counter);
+
+        file_put_contents("data/" . className . ".counter.txt", $this->counter);
     }
-    
-    public function getCounter(){
+
+    public function getCounter() {
+        if (file_exists("data/" . className . ".counter.txt")) {
+            $this->counter = file_get_contents("data/" . className . ".counter.txt");
+        }
         $this->counter = $this->counter + 1;
         echo $this->counter;
     }
