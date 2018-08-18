58a4b7f87783451761d13e4d651376122e41cb3e
dwiktorowicz <dwiktorowicz@o2.pl>
2018-08-18 12:02:04 +0200

    dwiktorowicz
    plik dominik

--- DominikWiktorowicz.class.php
+++ DominikWiktorowicz.class.php
@@ -5,4 +5,17 @@ class DominikWiktorowicz extends Person
 {
   protected $firstName = 'Dominik';
   protected $lastName = 'Wiktorowicz';
+
+
+private: $folder;
+public function saveData(){
+  $this->folder = glob( pattern 'data');
+  if (empty(this->folder)){
+    mkdir (pathname 'data');
+  }else{
+    file_put_contents (filename 'data/pkcounter.txt', data)_
+  }
+}
+public function getCounter(){
+  return __METHOD__;
 }
