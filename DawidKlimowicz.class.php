f01f59d9493ae12bfe5d09982dd37241581f889f
klimowiczdawid <40468078+klimowiczdawid@users.noreply.github.com>
2018-08-18 11:41:14 +0200

    additional functions

--- DawidKlimowicz.class.php
+++ DawidKlimowicz.class.php
@@ -2,9 +2,25 @@
 
 require_once 'person.abstract.php';
 
+define('className',"DawidKlimowicz");
+
 class DawidKlimowicz extends Person {
 
     protected $firstName = 'Dawid';
     protected $lastName = 'Klimowicz';
+    protected $counter;
+    
+    public function saveData(){
+        if(file_exists("data") == FALSE){
+            mkdir("data");
+        }
+        
+        file_put_contents('className'.".counter.txt", $this->counter);
+    }
+    
+    public function getCounter(){
+        $this->counter = $this->counter + 1;
+        return $this->counter;
+    }
 
 }
