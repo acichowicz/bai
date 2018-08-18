7a97996ff8a2ec97e421e368195ee86c3decc58e
dwiktorowicz <dwiktorowicz@o2.pl>
2018-08-18 10:59:57 +0200

    dwiktorowicz
    plik dominik

--- DominikWiktorowicz.class.php
+++ DominikWiktorowicz.class.php
@@ -1,15 +1,7 @@
-# bai
-BAI
-
-Tworzymy plik z klasą, która będzie miała nazwę składającą się z imienia i nazwiska bez polskich znaków
-AleksanderCichowicz.class.php - nazwa pliku
-class AleksanderCichowicz - nazwa klasy w środku
-każda klasa ma zawierać chronione zmienne firstName oraz lastName oraz domyślnie przypisane wartości odpowiadające waszym danym
-
 <?php
 require_once 'person.abstract.php';
 
-class DominikWiktorowicz extends Person 
+class DominikWiktorowicz extends Person
 {
   protected $firstName = 'Dominik';
   protected $lastName = 'Wiktorowicz';
