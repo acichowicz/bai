ff64d4a54cf058b9dd5934d22a19e40fe80b1458
Midgardsorm <chrusciel.krzysztof@gmail.com>
2018-08-18 12:32:07 +0200

    first commit on branch

--- KrzysztofChru.class.php
+++ KrzysztofChru.class.php
@@ -9,20 +9,7 @@ class KrzysztofChru extends Person  {
     
     protected $count = 0;
     
-    public function saveData()
-    {
-        if (!file_exists("Data")) { mkdir("Data", 0777); }
-      
-        $handle = fopen("Data/KrzysztofChru.counter.txt", 'w');
-        fwrite($handle, $this->count);
-     
-    }
-    
-    public function getCounter()
-    {
-        $this->count += 1;
-        return $this->count;
-    }
+
 }
 /* 
  * To change this license header, choose License Headers in Project Properties.
--- person.abstract.php
+++ person.abstract.php
@@ -4,6 +4,8 @@ require_once 'person.interface.php';
 abstract class Person implements PersonInterface {
 	protected $firstName;
 	protected $lastName;
+        
+        protected $count;
 	public function getFirstName() {
 		return $this->firstName;
 	}
@@ -11,4 +13,18 @@ abstract class Person implements PersonInterface {
 	public function getLastName() {
 		return $this->lastName;
 	}
+        
+        public function saveData()
+        {
+        if (!file_exists("Data")) { mkdir("Data", 0777); }
+      
+        $handle = fopen("Data/KrzysztofChru.counter.txt", 'w');
+        fwrite($handle, $this->count);
+        }
+    
+        public function getCounter()
+        {
+            $this->count += 1;
+            return $this->count;
+        }
 }
