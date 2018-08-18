6893f163b6a6aec8fab172f576283b4180ecb9a2
Konrad Maslowiec <kmaslowiec@gmail.com>
2018-08-18 12:48:11 +0200

    Final refactor
    done

--- KonradMaslowiec.class.php
+++ KonradMaslowiec.class.php
@@ -9,29 +9,8 @@ class KonradMaslowiec extends Person {
     protected $firstName = 'Konrad';
     protected $lastName = 'Maslowiec';
     protected $counter = 0;
-
-    public function saveData() {
-        if (!file_exists("data")) {
-            mkdir("data");
-        }
-
-        file_put_contents("data/" . className . ".counter.txt", $this->counter);
-    }
-
-    public function getCounter() {
-        if (file_exists("data/" . className . ".counter.txt")) {
-            $this->counter = file_get_contents("data/" . className . ".counter.txt");
-        }
-        $this->counter = $this->counter + 1;
-        echo $this->counter;
-    }
-
+    
 }
-
-    
-    
-
-
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
